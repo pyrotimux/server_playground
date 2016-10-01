cac4c8c21ee54f93444ad41fd86d85dee2910ccd
Pyrotimux <chaosmuskey@gmail.com>
2016-10-01 00:18:33 -0700

    Fixed a couple of misconceptions

--- preps/ubn_ansprep.sh
+++ preps/ubn_ansprep.sh
@@ -2,6 +2,7 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible python-pip python-dev build-essential 
 cp /vagrant/preps/hosts /etc/ansible/hosts
+cp /vagrant/preps/ansible.cfg /etc/ansible/ansible.cfg
 sudo pip install xmltodict
 sudo pip install pywinrm
 
